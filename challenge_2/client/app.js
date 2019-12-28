8b2497b1de66ff4b9782f42c8141a074a5041137
jihedabdelly <jihednew7@gmail.com>
2019-12-28 17:09:44 +0100

    challenge 2 change 1

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,9 @@
 
+console.log("wwwwiiiiiiwwww")
+document.getElementById('btn').addEventListener("click", function() {
+  var input = document.getElementById('npt').value
+
+})
+ console.log(input)
+
 

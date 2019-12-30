1f38646ad83cb4dbe75d0e57a9ec4557d677d6aa
jihedabdelly <jihednew7@gmail.com>
2019-12-30 18:53:07 +0100

    challenge 3 1st

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Personal extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name : '',
+      email : '',
+      password : ''
+    }
+  }
+
+  handleChange() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          Name : <input type="text" value={this.state.name}/>
+          Email : <input type="email" value={this.state.email}/>
+          PassWord : <input type="password" value={this.state.password}/>
+          <button onClick="">Next</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+checkOut=() =>{
+  return <form1/>
+};
+
+var form1 = ()=>(
+  <form>
+    Name : <input type="text"/>
+    Email : <input type= "email"/>
+    PassWord : <input type="password"/>
+    <button onClick="">Next</button>
+  </form>
+);
+
+var form2 = ()=>(
+  <form> Address
+    Line 1 : <input type="number"/> Line 2 : <input type="number"/>
+    City : <input type="text"/> State : <input type="text"/> ZIP Code : <input type="number"/>
+  </form>
+);
+
+var form3 = ()=>(
+  <form>
+    Credit Card Number : <input type="number"/>
+    Expiry Date : <input type="date"/>
+    CVV : <input type="number" size="3"/>
+    ZIP Code : <input type="number"/>
+  </form>
+);
+
+var button = <button onClick={checkOut}>CheckOut</button>;
 
 
+
+
+ReactDOM.render(
+  <Personal/>,
+  document.getElementById('app')
+);
+
+//export default Personal
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
 
+app.get('/',function(){
+  console.log('hiii')
+})
 
+app.listen(3000, function() {
+  console.log('Running !!!!')
+})
